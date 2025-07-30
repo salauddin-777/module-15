de8f34cc2ab9708a2dc539d5ce35ba64b1756c1a
salauddin-777 <mdsalauddinmina777@gmail.com>
2025-07-30 13:43:20 +0600

    Break and continue of loop

---
+++ break.js
@@ -0,0 +1,17 @@
+
+// for(let i = 0; i< 100; i++){
+//     console.log(i);
+//     if( i > 50){
+//         break;
+//     }
+// }
+
+let num = 60;
+
+while(num > 20){
+    if( num < 51){
+        break;
+    }
+    console.log(num);
+    num--;
+}
---
+++ continue.js
@@ -0,0 +1,24 @@
+
+
+// Continue - Skip this one- loop cholar somoy true hoile eyta skipp hoye jabe
+// Break - i am done with this code , loop end
+
+
+// for(let i = 1; i < 10; i++){
+//     if(i % 2 !==0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+let n = 0;
+
+while(n < 50){
+    n++;
+    if(n % 5 !==0){
+        continue;
+    }
+        console.log(n);
+    
+}
