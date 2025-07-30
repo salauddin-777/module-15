7e5829f15bf5fe21de58a86b629cfe2cb91b27ba
salauddin-777 <mdsalauddinmina777@gmail.com>
2025-07-30 13:48:52 +0600

    do while loop

---
+++ do-while.js
@@ -0,0 +1,25 @@
+
+
+
+// Do while Loop
+
+let n = 10;
+
+do{
+    console.log(n);
+    n++;
+}
+
+while(n < 5)
+
+
+
+// SIMPLE WHILE LOOP
+
+// let i = 10;
+// while(i < 5){
+//     console.log(n);
+//     i++;
+// }
+
+// OUTPUT- NONE
