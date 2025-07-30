4c2fad753288cc3d45a9c5bbcf93455425f89fce
salauddin-777 <mdsalauddinmina777@gmail.com>
2025-07-30 13:15:42 +0600

    third commit

---
+++ for-sum.js
@@ -0,0 +1,19 @@
+
+// let sum = 0;
+
+// for(let i = 11; i <=20; i++){
+//     sum = sum + i;
+//     console.log(i);
+// }
+
+// console.log('Some of Number:', sum);
+
+
+let i = 0;
+
+do{
+  console.log('One')
+  i++;
+
+}
+while(i<= 5)
